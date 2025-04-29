913cfe960b07e411c63167e61d6a92ddb172790f
Atheris <drac29673@gmail.com>
2025-04-29 11:32:37 +0530

    Update motor.c

--- PSDL/motor.c
+++ PSDL/motor.c
@@ -1,45 +1,38 @@
-#include<xc.h>
+#include <xc.h>
 #include <pic18f4550.h>
-unsigned char count=0;
 
-unsigned char TIMER, SPEED_UP;
+#define _XTAL_FREQ 8000000  // Define oscillator frequency for __delay_ms() if needed
 
-
-void timer2Init(void)
-{
-T2CON=0b00000010;
-PR2 = 0x95;
-}
-void delay(unsigned int time)
-{
-unsigned int i,j;
-for(i=0;i<time;i++);
-for(j=0;j<1000;j++);
+void timer2Init(void) {
+    T2CON = 0b00000010; // Timer2 ON, prescaler = 16
+    PR2 = 0x95;         // Set period register
+    TMR2ON = 1;         // Start Timer2
 }
 
+void delay(unsigned int time) {
+    while(time--) {
+        __delay_ms(1);  // Using built-in delay (assuming _XTAL_FREQ defined)
+    }
+}
 
 void main(void) {
-unsigned int i;
-TRISCbits.TRISC1=0;
-TRISCbits.TRISC2=0;
-LATCbits.LATC1=0;
-CCP1CON =0b00111100;
+    TRISCbits.TRISC1 = 0; // Set RC1 as output
+    TRISCbits.TRISC2 = 0; // Set RC2 as output
+    LATCbits.LATC1 = 0;   // Clear RC1
 
-CCPR1L=0x0F;
-timer2Init();
-TMR2ON=1;
+    CCP1CON = 0b00111100; // PWM mode
+    CCPR1L = 0x0F;        // Initial duty cycle
 
-while(1)
-{
-for(i=15;i<150;i++)
-{
-    CCPR1L=i;
-    delay(100);
-}
-for(i=150;i>15;i--)
-{
-    CCPR1L=i;
-    delay(100);
-}
-}
+    timer2Init();         // Initialize Timer2
+
+    while(1) {
+        for(unsigned int i = 15; i <= 150; i++) {
+            CCPR1L = i;
+            delay(1);
+        }
+        for(unsigned int i = 150; i >= 15; i--) {
+            CCPR1L = i;
+            delay(1);
+        }
+    }
 }
